1e7e128828bd17caafc489ac41c23e1cf59143a0
Đinh Nguyễn Quốc Bảo <39934512+K17-VP-1752007@users.noreply.github.com>
2021-04-28 23:17:02 +0700

    Create Congratulations prefab
    Prefabs, Animations

---
+++ Assets/Scripts/CongratsAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratsAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator prevAnimator;
+    [SerializeField] private Animator curAnimator;
+    [SerializeField] private string prevAnimationName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prevAnimator.GetCurrentAnimatorStateInfo(0).IsName(prevAnimationName))
+        {
+            curAnimator.SetTrigger("Appear");
+        }
+    }
+}
