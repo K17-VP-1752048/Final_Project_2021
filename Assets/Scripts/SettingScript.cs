9722e8a0a7eb4b2cd11bbbc8fd53f25512d35c8c
Đinh Nguyễn Quốc Bảo <39934512+K17-VP-1752007@users.noreply.github.com>
2021-05-20 22:08:52 +0700

    Create SettingPanel prefabs
    Prefabs, Scenes

---
+++ Assets/Scripts/SettingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingScript : MonoBehaviour
+{
+    [SerializeField] GameObject settingPanel;
+
+    public void OpenSettingPanel()
+    {
+        if(settingPanel != null)
+        {
+            settingPanel.SetActive(true);
+        }
+    }
+
+    public void CloseSettingPanel()
+    {
+        if(settingPanel != null)
+        {
+            settingPanel.SetActive(false);
+        }
+    }
+}
