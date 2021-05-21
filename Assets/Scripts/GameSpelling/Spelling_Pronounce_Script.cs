8e733b2a18417a348cd6d881f8bbffc9056e7e88
K17-VP-1752048 <40013974+K17-VP-1752048@users.noreply.github.com>
2021-05-21 21:44:00 +0700

    UP
    UP

---
+++ Assets/Scripts/GameSpelling/Spelling_Pronounce_Script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Spelling_Pronounce_Script : MonoBehaviour
+{
+    public UnityEvent downEvent;
+    void OnMouseDown()
+    {
+        //run event
+        downEvent?.Invoke();
+    }
+}
