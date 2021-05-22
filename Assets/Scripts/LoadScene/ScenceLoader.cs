8e246df011122629dba9aaed9f4ace0eb0a8cda5
K17-VP-1752038 <1752038@student.hcmus.edu.vn>
2021-05-22 16:52:06 +0700

    Topic scences
    UI edit

---
+++ Assets/Scripts/LoadScene/ScenceLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenceLoader : MonoBehaviour
+{
+    public void LoadTopicsScene()
+    {
+        SceneManager.LoadScene("Scenes/TopicsScene");
+    }
+    public void LoadCollectionScene()
+    {
+        SceneManager.LoadScene("Scenes/CollectionScene");
+    }
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("Scenes/MenuScene");
+    }
+}
