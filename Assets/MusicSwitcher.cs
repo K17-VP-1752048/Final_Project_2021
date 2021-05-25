0533f1dfe56067ad4cb586dfa6f3fa13a304338d
K17-VP-1752038 <1752038@student.hcmus.edu.vn>
2021-05-25 16:57:07 +0700

    BGMusic
    setting fix

---
+++ Assets/MusicSwitcher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSwitcher : MonoBehaviour
+{
+    public Sprite imgCheckBox;
+    public Sprite imgCheckBoxChecked;
+
+    private GameObject backgroundMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundMusic = GameObject.Find("BackgroundAudio");
+
+        AudioSource audioSource = backgroundMusic.GetComponent<AudioSource>();
+        if (audioSource.mute)
+        {
+            GetComponent<Image>().sprite = imgCheckBox;
+        }
+        else
+        {
+            GetComponent<Image>().sprite = imgCheckBoxChecked;
+        }
+    }
+
+    public void TurnOnAndOffMusic()
+    {
+        AudioSource audioSource = backgroundMusic.GetComponent<AudioSource>();
+
+        if (audioSource.mute)
+        {
+            GetComponent<Image>().sprite = imgCheckBoxChecked;
+            audioSource.mute = false;
+        }
+        else
+        {
+            GetComponent<Image>().sprite = imgCheckBox;
+            audioSource.mute = true;
+        }
+    }
+}
