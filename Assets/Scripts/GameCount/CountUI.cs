e005ee3a7c884a09130b06b5bc1ced29a7a7b4a1
K17-VP-1752048 <40013974+K17-VP-1752048@users.noreply.github.com>
2021-05-29 21:28:00 +0700

    up GameMatch + GameCount
    up

---
+++ Assets/Scripts/GameCount/CountUI.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountUI : MonoBehaviour
+{
+    [SerializeField] private List<NumberUI> options;
+    [SerializeField] private CountManager countManager;
+    [SerializeField] private GameObject showRes;
+    [SerializeField] private AudioClip bravo_audio;
+    [SerializeField] private AudioClip fail_audio;
+    [SerializeField] private TMP_Text questionInfoText;
+    [SerializeField] private TMP_Text questionAnimalText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChooseOption(Image opt)
+    {
+        for (int i = 0; i < options.Count; i++)
+        {
+            options[i].numberImg.color = new Color(0.8823529f, 0.8862745f, 0.6509804f, 1);
+        }
+        StartCoroutine(BlinkSelectedImg(opt));
+    }
+
+    //handle button verifier
+    public void HandleOptionChoose(GameObject obj)
+    {
+        foreach (Transform child in obj.transform)
+        {
+            if (child.GetComponentInChildren<Image>().color == new Color(1, 1, 0, 1))
+            {
+                HandleQuestion(child.GetComponentInChildren<Image>());
+                break;
+            }
+        }
+    }
+
+    public void HandleQuestion(Image ansImg)
+    {
+        bool val = countManager.HandleCount(ansImg.GetComponentInChildren<TMP_Text>().text);
+        if (val)
+        {
+            //set color to correct
+            StartCoroutine(BlinkCorrectImg(ansImg));
+        }
+        else
+        {
+            //set color to incorrect
+            StartCoroutine(BlinkWrongImg(ansImg));
+        }
+    }
+
+    //this give blink effect [if needed use or dont use]
+    IEnumerator BlinkSelectedImg(Image img)
+    {
+        img.color = Color.white;
+        yield return new WaitForSeconds(0.1f);
+        img.color = new Color(1, 1, 0, 1);
+        img.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(img.GetComponent<AudioSource>().clip.length + 2f);
+    }
+
+    //this give blink effect [if needed use or dont use]
+    IEnumerator BlinkWrongImg(Image img)
+    {
+        img.color = Color.white;
+        yield return new WaitForSeconds(0.1f);
+        img.color = Color.red;
+        //img.GetComponent<AudioSource>().Play();
+        gameObject.GetComponent<AudioSource>().clip = fail_audio;
+        gameObject.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(gameObject.GetComponent<AudioSource>().clip.length);
+        img.color = new Color(0.8823529f, 0.8862745f, 0.6509804f, 1);
+    }
+
+    //this give blink effect [if needed use or dont use]
+    IEnumerator BlinkCorrectImg(Image img)
+    {
+        img.color = Color.white;
+        yield return new WaitForSeconds(0.1f);
+        img.color = Color.green;
+        //img.GetComponent<AudioSource>().Play();
+        showRes.SetActive(true);
+        showRes.GetComponent<AudioSource>().clip = bravo_audio;
+        showRes.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(1f);
+        showRes.GetComponentInChildren<Image>().GetComponentInChildren<TMP_Text>().text = questionInfoText.text + " " + img.GetComponentInChildren<TMP_Text>().text.ToLower() + " " + questionAnimalText.text;
+    }
+}
---
+++ Assets/Scripts/GameCount/HandleNumber.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class HandleNumber : MonoBehaviour
+{
+    public UnityEvent downEvent;
+    void OnMouseDown()
+    {
+        //run event
+        downEvent?.Invoke();
+    }
+}
