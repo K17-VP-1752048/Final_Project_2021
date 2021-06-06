060846e34a2e4057aab4c0abfcdb1948b7a4c88f
K17-VP-1752038 <1752038@student.hcmus.edu.vn>
2021-06-06 23:16:06 +0700

    puzzle
    puzzle1

---
+++ Assets/Scripts/Puzzle/PuzzleControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleControl : MonoBehaviour
+{
+    [SerializeField] public string puzzleName;
+    public static bool win;
+    private static new string name;
+
+    private Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        win = false;
+        winText = GameObject.Find("TextWin").GetComponent<Text>();
+        winText.enabled = false;
+        name = puzzleName;
+    }
+
+    public static void CheckAnswer()
+    {
+        //bool flag = true;
+        for (int i = 0; i < 28; i++)
+        {
+            Transform pic = GameObject.Find("puzzle_" + name + "_" + i).GetComponent<Transform>();
+            if (pic.rotation.z != 0)
+            {
+                break;
+            }
+            win = true;
+        }
+
+        //if(flag)
+        //{
+        //    win = true;
+        //}
+    }
+
+    void Update()
+    {
+        if(win)
+            winText.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Puzzle/RotateToucher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToucher : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if(!PuzzleControl.win)
+        {
+            transform.Rotate(0f, 0f, 90f);
+            PuzzleControl.CheckAnswer();
+        }
+    }
+}
