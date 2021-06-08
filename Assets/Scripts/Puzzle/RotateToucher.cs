d6967e4600e988cd5140db4c35a92d2344d4e8b2
K17-VP-1752038 <1752038@student.hcmus.edu.vn>
2021-06-08 23:25:43 +0700

    Puzzle
    puzzle and loading UI

--- Assets/Scripts/Puzzle/RotateToucher.cs
+++ Assets/Scripts/Puzzle/RotateToucher.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class RotateToucher : MonoBehaviour
 {
+    [SerializeField] AudioSource puzzleSound;
     private void OnMouseDown()
     {
         if(PuzzleControl.continu)
         {
             transform.Rotate(0f, 0f, 90f);
+            puzzleSound.Play();
             PuzzleControl.CheckAnswer();
         }
     }
