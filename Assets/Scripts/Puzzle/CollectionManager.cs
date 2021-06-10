06a4b31091be6d4e94c6bc36ca3d2a948e581fd0
K17-VP-1752038 <1752038@student.hcmus.edu.vn>
2021-06-10 23:15:44 +0700

    collection + numberIntro
    grrr

---
+++ Assets/Scripts/Puzzle/CollectionManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionManager : MonoBehaviour
+{
+    [SerializeField] int numberOfTreasure;
+    public static int numberOfOpenTreasure;
+    private Canvas OpenTreasureCanvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = numberOfTreasure; i > 0; i--)
+        {
+            GameObject tmp = GameObject.Find("Puzzle" + i);
+            tmp.transform.Find("Image").GetComponent<Image>().enabled = false;
+
+            if (i > numberOfOpenTreasure + 1)
+            {
+                tmp.GetComponent<Image>().color = Color.grey;
+            }
+        }
+        OpenTreasureCanvas = GameObject.Find("CanvasOpenTreasure").GetComponent<Canvas>();
+        OpenTreasureCanvas.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Puzzle/TreasureManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureManager : MonoBehaviour
+{
+    //    private Animation keyFly = GameObject.Find("Key").GetComponent<Animation>();
+    private Canvas OpenTreasureCanvas;
+
+    private void Start()
+    {
+        OpenTreasureCanvas = GameObject.Find("CanvasOpenTreasure").GetComponent<Canvas>();
+
+    }
+
+    public void openATreasure()
+    {
+        string name = "Puzzle" + CollectionManager.numberOfOpenTreasure + 1;
+        StartCoroutine(openTreasureAnim(name));
+    }
+
+    IEnumerator openTreasureAnim(string objectName)
+    {
+        if (gameObject.name.Equals(objectName))
+        {
+ //           keyFly.Play();
+            CollectionManager.numberOfOpenTreasure++;
+            OpenTreasureCanvas.enabled = true;
+            yield return new WaitForSeconds(2f);
+
+            GameObject tmp = GameObject.Find(objectName);
+            tmp.GetComponent<Image>().sprite = null;
+            tmp.transform.Find("Image").GetComponent<Image>().enabled = true;
+
+            OpenTreasureCanvas.enabled = false;
+        }
+    }
+}
