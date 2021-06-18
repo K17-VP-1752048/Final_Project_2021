13cc1e3deedfea8eeb9eee183a3ad3117a6fbbd0
K17-VP-1752038 <1752038@student.hcmus.edu.vn>
2021-06-18 23:15:01 +0700

    puzzle sticker
    UI + Anim

--- Assets/Scripts/Collection/Puzzle/PuzzleImageManager.cs
+++ Assets/Scripts/Collection/Puzzle/PuzzleImageManager.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleImageManager : MonoBehaviour
 {
-    [SerializeField] string bgName;
     // Start is called before the first frame update
     void Start()
-    {
-        GameObject.Find(bgName).GetComponent<Animator>().Play("zoomIn");
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
