0338b3505f4cc7cdc6ff2bc8500e2dac086838b3
K17-VP-1752038 <1752038@student.hcmus.edu.vn>
2021-06-24 22:30:49 +0700

    Check sound in game
    Count, Find, PickObject

---
+++ Assets/Scripts/GameSoundChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSoundChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // check if Sound is OFF/ON
+        if (!KeepSoundPlay.state)
+        {
+            SetStateForSoundEffect(false);
+        }
+    }
+    private void SetStateForSoundEffect(bool state)
+    {
+        AudioSource[] sounds = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource s in sounds)
+        {
+            if (!s.clip.ToString().Equals("bgMusic (UnityEngine.AudioClip)"))
+            {
+                //Debug.Log("Turn off " + s.clip.ToString());
+                s.mute = !state;
+            }
+        }
+    }
+}
