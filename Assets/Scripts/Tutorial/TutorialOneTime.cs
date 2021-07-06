b94bc9675393f6985805fbb610bbb52f1d48e40e
K17-VP-1752048 <40013974+K17-VP-1752048@users.noreply.github.com>
2021-07-06 22:26:10 +0700

    update tutorial Game Match+Countnumber
    update

---
+++ Assets/Scripts/Tutorial/TutorialOneTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialOneTime : MonoBehaviour
+{
+    [SerializeField] private GameObject handGuide;
+    [SerializeField] private string nameGame;
+
+    private SaveLoadFile slf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initiate
+        slf = gameObject.AddComponent<SaveLoadFile>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tutorial for the first scene
+        if (!slf.CheckCompleteGame(nameGame) && (handGuide != null))
+        {
+            handGuide.SetActive(true);
+        }
+    }
+}
