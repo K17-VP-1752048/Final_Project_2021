3047fda9a7ce64f706577dfb6ada0ea3533ba3b4
K17-VP-1752048 <40013974+K17-VP-1752048@users.noreply.github.com>
2021-07-17 15:06:06 +0700

    delete update() in LoadValueKey.cs
    delete

--- Assets/Scripts/LoadingGame/LoadValueKey.cs
+++ Assets/Scripts/LoadingGame/LoadValueKey.cs
@@ -18,10 +18,4 @@ public class LoadValueKey : MonoBehaviour
         //get value key
         keyImg.GetComponentInChildren<TMP_Text>().text = slf.LoadKey().ToString();
     }
-
-    void Update()
-    {
-        //get value key
-        keyImg.GetComponentInChildren<TMP_Text>().text = slf.LoadKey().ToString();
-    }
 }
