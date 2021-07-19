5324d225edc44d4562f94ddf7ac602179ffafc9a
K17-VP-1752048 <40013974+K17-VP-1752048@users.noreply.github.com>
2021-07-19 16:40:03 +0700

    Setting Trophies
    update

---
+++ Assets/Scripts/LoadingGame/ChangeTrophies.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTrophies : MonoBehaviour
+{
+    [SerializeField] GameObject bronze, champion, hero;
+
+    private SaveLoadFile slf;
+    private int box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initiate
+        slf = gameObject.AddComponent<SaveLoadFile>();
+
+        box = slf.LoadBox();
+        if (box > 2 && box < 6)
+        {
+            bronze.gameObject.SetActive(false);
+            champion.gameObject.SetActive(true);
+        }
+        else if (box >= 6)
+        {
+            bronze.gameObject.SetActive(false);
+            hero.gameObject.SetActive(true);
+        }
+    }
+
+    /* // Update is called once per frame
+     void Update()
+     {
+         box = slf.LoadBox();
+         if (box > 2 && box < 6)
+         {
+             bronze.gameObject.SetActive(false);
+             champion.gameObject.SetActive(true);
+         }
+         else if (box >= 6)
+         {
+             bronze.gameObject.SetActive(false);
+             hero.gameObject.SetActive(true);
+         }
+     }*/
+}
