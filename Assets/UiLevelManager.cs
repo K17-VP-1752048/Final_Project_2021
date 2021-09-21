c3ff256f45968fc96a4d059e3bf826db7765955f
Đinh Nguyễn Quốc Bảo <39934512+K17-VP-1752007@users.noreply.github.com>
2021-09-21 09:00:30 +0700

    Change LoadingScene + MenuScene UI

---
+++ Assets/UiLevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiLevelManager : MonoBehaviour
+{
+    [SerializeField] GameObject levelBtnPrefab;
+    [SerializeField] int numberOfLevels = 8;
+    [SerializeField] int numberOfLevelsUnlocked = 3;
+    [SerializeField] Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject levelBtn;
+        for(int i = 0; i < numberOfLevels; i++)
+        {
+            levelBtn = Instantiate(levelBtnPrefab, transform);
+            if(i < numberOfLevelsUnlocked)
+            {
+                levelBtn.transform.GetComponentInChildren<TMP_Text>().text = "" + (i + 1);
+                levelBtn.transform.GetComponent<Image>().color = color;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
