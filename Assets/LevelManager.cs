d3e1fafbf07e66cc29f89ef683ab2380583c240d
Đinh Nguyễn Quốc Bảo <39934512+K17-VP-1752007@users.noreply.github.com>
2021-09-21 16:47:58 +0700

    Create Number level 1 + 2 UI

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private int correctAns = 0;
+    [SerializeField] StarRate starRateBar;
+    [SerializeField] DataAnimalsScriptable dataNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckAnswer()
+    {
+        correctAns++;
+        starRateBar.UpdateStarRate(correctAns);
+        
+    }
+
+    public void CreateCardData()
+    {
+
+    }
+}
---
+++ Assets/StarRate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarRate : MonoBehaviour
+{
+    private Image progressBar;
+    private GameObject stars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressBar = transform.GetChild(0).Find("Fill").GetComponent<Image>();
+        stars = transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartCoroutine(UpdateProgressBar());
+    }
+
+    public void UpdateStarRate(int starAchieved)
+    {
+        float fillAmmount = 0;
+        if(starAchieved == 1)
+        {
+            fillAmmount = 0.25f;
+        }
+        if(starAchieved == 2)
+        {
+            fillAmmount = 0.6f;
+        }
+        if(starAchieved >= 3)
+        {
+            fillAmmount = 1f;
+        }
+        progressBar.fillAmount = fillAmmount;
+        for(int i = 0; i < starAchieved; i++)
+        {
+            stars.transform.GetChild(i).GetComponent<Animator>().SetTrigger("Unlocked");
+        }
+    }
+
+    
+}
